50abc1469531cde9f99df13855798ac50a71e905
Walkoszes <lizzinowl@gmail.com>
2024-09-06 17:11:24 +0300

    Added ability to comment posts

--- MyBlog/blog/models.py
+++ MyBlog/blog/models.py
@@ -3,6 +3,18 @@ from django.utils import timezone
 import datetime
 
 # Create your models here.
+from django.db import models
+from django.utils import timezone
+
+class Comment(models.Model):
+    author_name = models.CharField(max_length=100)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    post = models.ForeignKey("Post", on_delete=models.CASCADE, related_name="comments")
+
+    def __str__(self):
+        return f"Comment by {self.author_name} on {self.post.title}"
+
 class Author(models.Model):
     name = models.CharField(max_length=100)
     bio = models.TextField()
--- MyBlog/blog/views.py
+++ MyBlog/blog/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post, Author
+from .models import Post, Author, Comment
 
 # Create your views here.
 def post_list(request):
     posts = Post.objects.all()
-    return render(request, 'post_list.html', {'posts': posts})
+    return render(request, "post_list.html", {"posts": posts})
 
 def post_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
-    return render(request, 'post_detail.html', {'post': post})
+    return render(request, "post_detail.html", {"post": post})
 
 def posts_by_author(request, author_id):
     author = get_object_or_404(Author, pk=author_id)
     posts = Post.objects.filter(author=author)
-    return render(request, 'posts_by_author.html', {'author': author, 'posts': posts})
+    return render(request, "posts_by_author.html", {"author": author, "posts": posts})
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    comments = post.comments.all()
+    return render(request, "post_detail.html", {"post": post, "comments": comments})
